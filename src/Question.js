1fa7c3f734ce16362c60f77b540fedee315d0192
DylanParenzee <dylanparenzee@hotmail.com>
2022-08-23 08:46:02 +1000

    finshed fuctionality for toggle display of question answers

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Question = () => {
-  return <h3>question component</h3>;
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const toggle = () => {
+    setShowInfo(!showInfo);
+  };
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button onClick={toggle} className="btn">
+          {showInfo ? "-" : "+"}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
